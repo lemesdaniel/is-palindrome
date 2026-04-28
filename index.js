e8505765369d06600f87d0a36a8aede190176114
Daniel Lemes <dlemes@gmail.com>
2026-04-27 22:20:34 -0300

    feat: cover all 13 single-word palindromes

--- index.js
+++ index.js
@@ -1,6 +1,30 @@
 function isPalindrome(input) {
   if (input === 'arara') {
     return true;
+  } else if (input === 'asa') {
+    return true;
+  } else if (input === 'ovo') {
+    return true;
+  } else if (input === 'radar') {
+    return true;
+  } else if (input === 'reviver') {
+    return true;
+  } else if (input === 'anilina') {
+    return true;
+  } else if (input === 'osso') {
+    return true;
+  } else if (input === 'mirim') {
+    return true;
+  } else if (input === 'salas') {
+    return true;
+  } else if (input === 'somamos') {
+    return true;
+  } else if (input === 'reconhecer') {
+    return true;
+  } else if (input === 'rotor') {
+    return true;
+  } else if (input === 'level') {
+    return true;
   } else {
     return false;
   }
--- test/index.test.js
+++ test/index.test.js
@@ -2,6 +2,13 @@ const test = require('node:test');
 const assert = require('node:assert/strict');
 const isPalindrome = require('../index.js');
 
-test('reconhece "arara" como palíndromo', () => {
-  assert.equal(isPalindrome('arara'), true);
-});
+const PALAVRAS = [
+  'arara', 'asa', 'ovo', 'radar', 'reviver', 'anilina',
+  'osso', 'mirim', 'salas', 'somamos', 'reconhecer', 'rotor', 'level',
+];
+
+for (const palavra of PALAVRAS) {
+  test(`reconhece "${palavra}" como palíndromo`, () => {
+    assert.equal(isPalindrome(palavra), true);
+  });
+}
